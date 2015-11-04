e1a90dd2347474869fd70ce3f9f97dc00978c274
mjremijan <mjremijan@yahoo.com>
2015-11-04 15:15:31 -0600

    1st commit

---
+++ ferris-cdi-shared/src/main/java/org/ferris/cdi/shared/AccountService.java
@@ -0,0 +1,12 @@
+package org.ferris.cdi.shared;
+
+/**
+ *
+ * @author <a href="mailto:mjremijan@yahoo.com">Michael Remijan</a>
+ */
+public class AccountService {
+
+    public String getAccountId() {
+        return "12345-abcde";
+    }
+}
