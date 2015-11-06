bd47397c3d8950c6094423c747ddf1e4acf7544c
mjremijan <mjremijan@yahoo.com>
2015-11-06 11:51:43 -0600

    Added log4j to hid Weld logging Added powerpoint presentation

--- ferris-cdi-research/src/main/java/org/ferris/cdi/research/Main.java
+++ ferris-cdi-research/src/main/java/org/ferris/cdi/research/Main.java
@@ -1,6 +1,7 @@
 package org.ferris.cdi.research;
 
 import javax.enterprise.inject.spi.CDI;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -8,16 +9,18 @@ import javax.enterprise.inject.spi.CDI;
  */
 public class Main {
 
+    public static final Logger log = Logger.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.printf(">>> ENTER Main#main\n");
+        log.debug(">>> ENTER Main#main\n");
         try (CDI<Object> cdi = CDI.getCDIProvider().initialize()) {
             // start the container, retrieve a bean and do work with it
-            System.out.printf(">>> CDI bootstrap complete. \n");
+            log.debug(">>> CDI bootstrap complete. \n");
             
             // get my first bean
             MyBean myBean = cdi.select(MyBean.class).get();
-            System.out.printf(">>> MyBean name=\"%s\"\n", myBean.getName());
-            System.out.printf(">>> MyBean accountId=\"%s\"\n", myBean.getAccountId());
+            log.debug(String.format(">>> MyBean name=\"%s\"\n", myBean.getName()));
+            log.debug(String.format(">>> MyBean accountId=\"%s\"\n", myBean.getAccountId()));
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
