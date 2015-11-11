11a20a1ecf1717dd796a4860acea77c52fcc57bc
Michael Remijan <mjremijan@yahoo.com>
2015-11-11 13:11:23 -0600

    search and replace

--- ferris-cdi-research/src/main/java/org/ferris/cdi/research/lifecycle/LifecyleExample.java
+++ ferris-cdi-research/src/main/java/org/ferris/cdi/research/lifecycle/LifecyleExample.java
@@ -8,7 +8,6 @@ import javax.annotation.PreDestroy;
  * @author Michael Remijan mjremijan@yahoo.com @mjremijan
  */
 public class LifecyleExample {
-
     @PostConstruct
     protected void postConstruct() {
         System.out.printf(">>>> LifeCycleExample Post Construct\n");
